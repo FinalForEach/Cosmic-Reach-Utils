2bcfb4f76ac695af6af66b91ee64ad82cfa204d0
FinalForEach <finalforeach@gmail.com>
2026-06-14 15:27:36 +0100

    Quick load params

--- src/main/java/finalforeach/cosmicreach/util/GameProperties.java
+++ src/main/java/finalforeach/cosmicreach/util/GameProperties.java
@@ -6,4 +6,6 @@ public class GameProperties
 	public static boolean isClient = true;
 	public static boolean logPackets = false;
 	public static String projectName;
+	public static String joinServerAddress;
+	public static String joinWorldName;
 }
